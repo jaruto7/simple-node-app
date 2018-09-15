7c930732fcef33d5408e0616d1ddee714101e345
Jarek Kajko <jaruto7@gmail.com>
2018-09-15 15:09:57 +0200

    Fix code for simple Node app in program.js

--- program.js
+++ program.js
@@ -9,32 +9,29 @@ process.stdin.on( 'readable', function() {
     if( input !== null ) {
         // Dopisz do zmiennej odczyt wejscia wartosci nastepnie zamien na string i usun odstepy
         var instruction = input.toString().trim();
-        // Sprawdz czy wartosc jest rowna stringowi
-        if( instruction === '/exit' ) {
-            // Wyswietl komunikat zapisu
-            process.stdout.write( 'Quitting app!\n' );
-            // Zakoncz proces i zamknij aplikacje 
-            process.exit();
-        } else {
-                // Jesli uzytkownik wpisal inna wartosc, wyswietl komunikat o bledzie
-                process.stderr.write( 'Wrong instruction!\n' );    
-        }
         // Jesli uzytkownik wpisal poprawna wartosc, wyswietl wynik
         switch( instruction ) {
-            // Ustaw wartosc w postaci stringa
+            // Sprawdz czy wartosc jest rowna stringowi
+            case '/exit':
+                // Wyswietl komunikat zapisu
+                process.stdout.write( 'Quitting app!\n' );
+                // Zakoncz proces i zamknij aplikacje 
+                process.exit();
+                break;
+            // Sprawdz czy wartosc jest rowna stringowi
             case 'lang':
                 // Wyswietl jezyk Node.JS
                 process.stdout.write( 'Node.JS language: ' + process.env.lang );
                 // Zatrzymaj proces
                 break;
-            // Ustaw wartosc w postaci stringa    
+            // Sprawdz czy wartosc jest rowna stringowi    
             case 'ver':
                 // wyswietl wersje Node.JS
                 process.stdout.write( 'Node.JS version: ' + process.versions.node );
                 // Zatrzymaj proces
                 break;
             // W przeciwnym razie gdy uzytkownik wpisal zla wartosc, wyswietl komunikat o bledzie
-            default: process.stderr.write( 'Theres nothing here' );    
+            default: process.stderr.write( 'Wrong instruction!\n' );    
         }          
     }
 });
